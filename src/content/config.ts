599d55382b3288a9108b8a86970d071825b47175
Sandipan Biswas <sandipanb223@gmail.com>
2025-07-04 07:29:20 +0000

    refactor: update project metadata and descriptions; make repository field optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,10 +5,9 @@ const projectCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    repository: z.string(),
+    repository: z.string().optional(),
     url: z.string().url().optional(),
     date: z.date().optional(),
-    published: z.boolean(),
   }),
 });
 
